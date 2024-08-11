550bd723d9ea1826186b3ac7a93a69b71632ddc2
Fernando Orozco <jose.acciones.gt@gmail.com>
2024-08-10 19:32:13 -0600

    added a new way of getting movies by id and category using services

--- routers/movie.py
+++ routers/movie.py
@@ -8,6 +8,7 @@ from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
 from middlewares.jwt_bearer import JWTBearer
 from fastapi.responses import HTMLResponse
+from services.movie import MovieService
 
 movie_router = APIRouter() 
 
@@ -105,7 +106,7 @@ def message():
 def get_movies() -> List[Movie]:
 
     db = Session()                                                                      # Creo la sesión de la base de datos
-    result = db.query(MovieModel).all()                                                 # Acá estoy obteniendo todos los registros de la tabla movies
+    result = MovieService(db).get_movies()                                              # Acá estoy obteniendo todos los registros de la tabla movies
 
     return JSONResponse(status_code=200, content=jsonable_encoder(result))              # Acá estoy retornando los registros de la tabla movies, uso jsonable_encoder para convertir los objetos de la base de datos a diccionarios
 
@@ -117,7 +118,7 @@ def get_movie_by_id(id: int = Path(ge=1, le=2000)) -> Movie:
 
     db = Session()                                    
     
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()                                  # Acá estoy obteniendo el registro de la tabla movies que tenga el id que viene en la URL y lo guardo en la variable result solo el primer registro que encuentre
+    result = MovieService(db).get_movie(id)                                                              # Acá estoy obteniendo el registro de la tabla movies que tenga el id que viene en la URL
 
     if not result:
         return JSONResponse(status_code = 404, content = {'message': 'Movie not found'})               # Acá estoy retornando un mensaje de error si no se encuentra el registro
@@ -130,7 +131,7 @@ def get_movie_by_category(category: str = Query(min_length=5, max_length=20), ye
 
     db = Session()                                                                                                  # Creo la sesión de la base de datos
 
-    result = db.query(MovieModel).filter(MovieModel.category == category).all()                                     # Acá estoy obteniendo todos los registros de la tabla movies que tengan la categoría que viene en la URL
+    result = MovieService(db).get_movie_by_category(category)                                                        # Acá estoy obteniendo los registros de la tabla movies que tengan la categoría que viene en la URL
 
     if not result:
         return JSONResponse(status_code = 404, content = {'message': 'Movie not found by category'})                # Acá estoy retornando un mensaje de error si no se encuentra el registro
